a3445c039e7a10cb9777c2b607da3d84945ab86f
de4thb0rn <pedroapferreirass@gmail.com>
2024-05-05 14:30:51 -0300

    Implementando mudanças em ArquivoLivro

--- TP2/ArquivoLivro.java
+++ TP2/ArquivoLivro.java
@@ -1,8 +1,58 @@
+import aed3.ListaInvertida;
 
-public class ArquivoLivro extends Arquivo<Livro>{
+public class ArquivoLivro extends Arquivo<Livro> {
 
-    ArquivoLivro(String na) throws Exception{
-        super(na,Livro.class.getConstructor());
+    private ListaInvertida listaInvertida;
+
+    ArquivoLivro(String na) throws Exception {
+        super(na, Livro.class.getConstructor());
+        try {
+            listaInvertida = new ListaInvertida(4, "SakasamanoLista", "Blocos");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-  
+
+    public int create(Livro novo) {
+        //Código original
+        int resp = this.create(novo);
+
+        //Uso da lista invertida
+        try {
+            listaInvertida.create(novo.getTitulo(), novo.getID());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return resp;
+    }
+
+    public boolean update(Livro novoLivro){
+        //Código original
+        boolean resp = this.update(novoLivro);
+
+        //Uso da lista invertida
+        try {
+            //Implementar update
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return resp;
+    }
+
+    public boolean delete(int id){
+        //Uso da lista invertida
+        //A ser implementado
+        //Código original
+        boolean resp = this.delete(id);
+        return resp;
+    }
+
+    public String pesquisa(String alvo){
+        String resp = "";
+
+
+        return resp;
+    }
+
 }
