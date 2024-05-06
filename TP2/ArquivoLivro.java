76884dbc3e04c76d6cd0917577e1bd68c3443ceb
pedroapferreirass@gmail.com <pedroapferreirass@gmail.com>
2024-05-06 11:48:25 -0300

    Andando para funcionar com multiplas palavras

--- TP2/ArquivoLivro.java
+++ TP2/ArquivoLivro.java
@@ -1,16 +1,30 @@
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import aed3.ListaInvertida;
 
 public class ArquivoLivro extends Arquivo<Livro> {
 
     private ListaInvertida listaInvertida;
+    private List<String> stopwords;
 
     ArquivoLivro(String na) throws Exception {
         super(na, Livro.class.getConstructor());
         try {
             listaInvertida = new ListaInvertida(4, "SakasamanoLista", "Blocos");
+            String stwrds = "de a o que e do da em um para é com não uma os no se na por mais as dos como mas foi ao ele das tem à seu sua ou ser quando muito há nos já está eu também só pelo pela até isso ela entre era depois sem mesmo aos ter seus quem nas me esse eles estão você tinha foram essa num nem suas meu às minha têm numa pelos elas havia seja qual será nós tenho lhe deles essas esses pelas este fosse dele tu te vocês vos lhes meus minhas teu tua teus tuas nosso nossa nossos nossas dela delas esta estes estas aquele aquela aqueles aquelas isto aquilo estou está estamos estão estive esteve estivemos estiveram estava estávamos estavam estivera estivéramos esteja estejamos estejam estivesse estivéssemos estivessem estiver estivermos estiverem hei há havemos hão houve houvemos houveram houvera houvéramos haja hajamos hajam houvesse houvéssemos houvessem houver houvermos houverem houverei houverá houveremos houverão houveria houveríamos houveriam sou somos são era éramos eram fui foi fomos foram fora fôramos seja sejamos sejam fosse fôssemos fossem for formos forem serei será seremos serão seria seríamos seriam tenho tem temos tém tinha tínhamos tinham tive teve tivemos tiveram tivera tivéramos tenha tenhamos tenham tivesse tivéssemos tivessem tiver tivermos tiverem terei terá teremos terão teria teríamos teriam";
+            String normalizada = Normalizer.normalize(stwrds, Normalizer.Form.NFD);
+            Pattern padrao = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+            normalizada = padrao.matcher(normalizada).replaceAll("");
+            String[] partes = normalizada.split(" ");
+            stopwords = new ArrayList<>();
+
+            for(String s: partes){
+                stopwords.add(s);
+            }
+
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -23,7 +37,16 @@ public class ArquivoLivro extends Arquivo<Livro> {
             resp = this.create(novo);
 
             // Uso da lista invertida
-            listaInvertida.create(novo.getTitulo(), novo.getID());
+            String normalizada = Normalizer.normalize(novo.getTitulo(), Normalizer.Form.NFD);
+            Pattern padrao = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+            normalizada = padrao.matcher(normalizada).replaceAll("");
+            String[] partes = normalizada.split(" ");
+            
+            for(String s : partes){
+                if(!stopwords.contains(s))
+                listaInvertida.create(s, novo.getID());
+            }
+            
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -41,8 +64,24 @@ public class ArquivoLivro extends Arquivo<Livro> {
             resp = this.update(novoLivro);
 
             // Uso da lista invertida
-            listaInvertida.delete(tmp.getTitulo(), tmp.getID());
-            listaInvertida.create(novoLivro.getTitulo(), novoLivro.getID());
+            String normalizada = Normalizer.normalize(tmp.getTitulo(), Normalizer.Form.NFD);
+            Pattern padrao = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+            normalizada = padrao.matcher(normalizada).replaceAll("");
+            String[] partes = normalizada.split(" ");
+            for(String s : partes){
+               listaInvertida.delete(s, tmp.getID()); 
+            }
+
+            normalizada = Normalizer.normalize(novoLivro.getTitulo(), Normalizer.Form.NFD);
+            padrao = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+            normalizada = padrao.matcher(normalizada).replaceAll("");
+            partes = normalizada.split(" ");
+
+            for(String s : partes){
+                if(!stopwords.contains(s)){
+                    listaInvertida.create(s, novoLivro.getID());
+                }
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -58,7 +97,14 @@ public class ArquivoLivro extends Arquivo<Livro> {
             resp = this.delete(id);
 
             // Uso da lista invertida
-            listaInvertida.delete(tmp.getTitulo(), tmp.getID());
+            String normalizada = Normalizer.normalize(tmp.getTitulo(), Normalizer.Form.NFD);
+            Pattern padrao = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+            normalizada = padrao.matcher(normalizada).replaceAll("");
+            String[] partes = normalizada.split(" ");
+            for(String s : partes){
+               listaInvertida.delete(s, tmp.getID()); 
+            }
+            
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -68,29 +114,56 @@ public class ArquivoLivro extends Arquivo<Livro> {
     public Livro[] pesquisa(String alvo) {
         String[] partido = alvo.split(" ");
         int[] tmparray;
+        int[] inserir = new int[55];
         Livro[] resp = null;
         Livro tmp;
 
         List<Livro> listinha = new ArrayList<>();
         try {
-            //For para buscar todas as palavras da string de busca
+            // For para buscar todas as palavras da string de busca
             for (String buscar : partido) {
+
                 tmparray = listaInvertida.read(buscar);
-                //Se o item ainda não está presente na Lista,então ele é adicionado
-                for(int item: tmparray){
+                
+                
+            }
+            // Se o item ainda não está presente na Lista,então ele é adicionado
+            for (int item : inserir) {
 
                     tmp = this.read(item);
-                    if(!listinha.contains(tmp)){
+                    if (!listinha.contains(tmp)) {
                         listinha.add(tmp);
                     }
 
-                }
             }
-            //Então a lista é convertida para um Array e retornada
-            resp = (Livro[]) listinha.toArray();
+            resp = new Livro[listinha.size()];
+            // Então a lista é convertida para um Array e retornada
+            for (int i = 0; i < listinha.size(); i++) {
+                resp[i] = listinha.get(i);
+            }
+
         } catch (Exception e) {
             System.out.println(e);
         }
+        return resp;
+    }
+
+    private int[] intersection(int[] a, int[] b){
+        List<Integer> listinha = new ArrayList<>();
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < b.length; j++){
+                if(a[i] == b[j] && !listinha.contains(a[i])){
+                    listinha.add(a[i]);
+                }
+            }
+        }
+
+        int[] resp = new int[listinha.size()];
+
+        for(int i = 0; i < listinha.size(); i++){
+            resp[i] = listinha.get(i);
+        }
+
         return resp;
     }
 
--- TP2/Principal.java
+++ TP2/Principal.java
@@ -7,13 +7,13 @@ public class Principal {
         File f = new File("dados/livros.db");
         f.delete();
         ArquivoLivro arqTeste;
-        Livro l1 = new Livro(-1, "9788563560278", "Odisseia", 15.99F);
+        Livro l1 = new Livro(-1, "9788563560278", "Bleach CFYW", 15.99F);
         Livro l2 = new Livro(-1, "9788584290482", "Ensino Híbrido", 39.90F);
-        Livro l3 = new Livro(-1, "9786559790005", "Modernidade L[iquida]", 48.1F);
-        Livro l4 = new Livro(-1, "9788582714911", "Memória", 55.58F);
-        Livro l5 = new Livro(-1, "9786587150062", "Com Amor", 48.9F);
-        Livro l6 = new Livro(-1, "9786587150063", "Bleach", 31.0F);
-        Livro l7 = new Livro(-1, "9786587150063", "Bleach Thousand Year Blood War", 31.0F);
+        Livro l3 = new Livro(-1, "9786559790005", "Ensino moderno", 48.1F);
+        Livro l4 = new Livro(-1, "9788582714911", "Memoria", 55.58F);
+        Livro l5 = new Livro(-1, "9786587150062", "Memoria moderno", 48.9F);
+        Livro l6 = new Livro(-1, "9786587150063", "Bleach", 65.0F);
+        Livro l7 = new Livro(-1, "9786587150063", "Bleach Thousand Year Blood War", 65.0F);
         Livro livre;
         Livro[] resposta;
         int id1, id2, id3, id4, id5, id6, id7;
@@ -21,63 +21,68 @@ public class Principal {
         try {
             arqTeste = new ArquivoLivro("dados/livros.db");
 
-            id1 = arqTeste.create(l1);
+            id1 = arqTeste.createLivro(l1);
             MyIO.println("Livro criado com o ID: " + id1);
 
-            id2 = arqTeste.create(l2);
+            id2 = arqTeste.createLivro(l2);
             MyIO.println("Livro criado com o ID: " + id2);
 
-            id7 = arqTeste.create(l7);
+            id7 = arqTeste.createLivro(l7);
             MyIO.println("Livro criado com o ID: " + id7);
 
-            id4 = arqTeste.create(l4);
+            id4 = arqTeste.createLivro(l4);
             MyIO.println("Livro criado com o ID: " + id4);
 
-            id5 = arqTeste.create(l5);
+            id5 = arqTeste.createLivro(l5);
             MyIO.println("Livro criado com o ID: " + id5);
 
-            if (arqTeste.delete(id2))
+            if (arqTeste.deleteLivro(id2))
                 MyIO.println("Livro de ID " + id2 + " excluído!");
             else
                 MyIO.println("Livro de ID " + id2 + " não encontrado!");
 
-            l4.setTitulo("A Memória");
-            if (arqTeste.update(l4))
+            l4.setTitulo("A Memoria");
+            if (arqTeste.updateLivro(l4))
                 MyIO.println("Livro de ID " + l4.getID() + " alterado!");
             else
                 MyIO.println("Livro de ID " + l4.getID() + " não encontrado!");
 
-            if (arqTeste.delete(id7))
-                MyIO.println("Livro de ID " + id7 + " excluído!");
-            else
-                MyIO.println("Livro de ID " + id7 + " não encontrado!");
+            /*
+             * if (arqTeste.deleteLivro(id7))
+             * MyIO.println("Livro de ID " + id7 + " excluído!");
+             * else
+             * MyIO.println("Livro de ID " + id7 + " não encontrado!");
+             */
 
-            id3 = arqTeste.create(l3);
+            id3 = arqTeste.createLivro(l3);
             MyIO.println("Livro criado com o ID: " + id3);
 
-            l4.setTitulo("A Memoria 2");
-            if (arqTeste.update(l4))
-                MyIO.println("Livro de ID " + l4.getID() + " alterado!");
-            else
-                MyIO.println("Livro de ID " + l4.getID() + " não encontrado!");
+            /*
+             * l4.setTitulo("A Memoria 2");
+             * if (arqTeste.update(l4))
+             * MyIO.println("Livro de ID " + l4.getID() + " alterado!");
+             * else
+             * MyIO.println("Livro de ID " + l4.getID() + " não encontrado!");
+             */
+            id6 = arqTeste.createLivro(l6);
 
-            id6 = arqTeste.create(l6);
             MyIO.println("Livro criado com o ID: " + id6);
 
-            livre = arqTeste.read(id1);
+            livre = arqTeste.read(id7);
             MyIO.println(livre.toString());
-
-            livre = arqTeste.read(id3);
-            MyIO.println(livre.toString());
-
-            livre = arqTeste.read(id4);
-            MyIO.println(livre.toString());
-
-            livre = arqTeste.read(id5);
-            MyIO.println(livre.toString());
-
-            livre = arqTeste.read(id6);
-            MyIO.println(livre.toString());
+            /*
+             * livre = arqTeste.read(id3);
+             * MyIO.println(livre.toString());
+             * 
+             * livre = arqTeste.read(id4);
+             * MyIO.println(livre.toString());
+             * 
+             * livre = arqTeste.read(id5);
+             * MyIO.println(livre.toString());
+             * 
+             * livre = arqTeste.read(id6);
+             * MyIO.println(livre.toString());
+             */
 
             String buscar = MyIO.readLine("Faca a busca: ");
 
